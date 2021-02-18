a0273b417bb93a1cd034548cb9f5293b31bb506c
abhinav2525 <56315478+abhinav2525@users.noreply.github.com>
2021-02-18 16:52:16 +0530

    Update caesarcipher.cpp

--- caesarcipher.cpp
+++ caesarcipher.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string.h>
+using namespace std;
 
 void caesar(char pt[], int ks);
 
@@ -8,8 +9,8 @@ int  main()
 
 char plaintext[30];
 int keyshift;
-std::cout<<"\n[~]Enter the plain text:"<<plaintext;
-std::cout<<"\n[~]Enter the number of keyshift:"<<keyshift;
+cout<<"\n[~]Enter the plain text:";cin>>plaintext;
+cout<<"\n[~]Enter the number of keyshift:";cin>>keyshift;
 caesar(plaintext,keyshift);
 
 return 0;
@@ -17,13 +18,13 @@ return 0;
 
 void caesar(char pt[], int ks)
 {
- std::string  result;
+ string  result;
  for(int i=0;i<strlen(pt);i++)
     { if(isupper(pt[i]))
         result += char(int(pt[i]+ks-65) % 26 + 65);
         else
         result += char(int(pt[i]+ks-97) % 26 + 97);
      }
- std::cout<<"[*]\nCaesar ciphertext:"<<result;
+ cout<<"[*]\nCaesar ciphertext:"<<result;
 }
 
